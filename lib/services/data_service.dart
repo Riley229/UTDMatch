466d99f911d3cb3566d75959beb4176f384db200
thoms <thomsoncamden@gmail.com>
2021-11-13 16:52:54 -0600

    Added currentUser set and create

--- lib/services/data_service.dart
+++ lib/services/data_service.dart
@@ -4,13 +4,45 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:utdtutors/models/app_user.dart';
 
 class DataService {
-  final _database = FirebaseFirestore.instance;
+  late final FirebaseFirestore _database;
+  late final CollectionReference _userCollection;
+
+  StreamSubscription? _currentUserSubscription;
+  AppUser? currentUser;
 
   late final StreamController _userStateController;
   late final Stream<AppUser?> userState;
 
   DataService() {
+    _database = FirebaseFirestore.instance;
+    _userCollection = _database.collection('users');
+
     _userStateController = StreamController<AppUser?>.broadcast();
     userState = _userStateController.stream as Stream<AppUser?>;
+  }
+
+  Future createUser({
+    required String id,
+    required String name,
+  }) async {
+    DocumentReference user = _userCollection.doc(id);
+
+    await user.set({
+      'id': id,
+      'name': name,
+    });
+  }
+
+  void setCurrentUser(String? id) {
+    _currentUserSubscription?.cancel();
+    currentUser = null;
+
+    if (id != null) {
+      DocumentReference user = _userCollection.doc(id);
+
+      user.snapshots().listen((document) {
+        currentUser = AppUser.fromFirestore(document);
+      });
+    }
   }
 }
