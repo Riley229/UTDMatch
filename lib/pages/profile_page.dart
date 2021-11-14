242a8b9412f7f21eb7d496ccd429ea20ee3d070b
Anthuony <brianpham907@gmail.com>
2021-11-13 18:17:49 -0600

    sign out button

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:utdtutors/services/auth_service.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -11,6 +13,16 @@ class _ProfilePageState extends State<ProfilePage> {
   final profileName = 'Osbaldo Waldo';
   final profileImageUrl =
       'http://i.ebayimg.com/images/g/Tw4AAOxyTjNSgMig/s-l500.jpg';
+  final profileClassification = 'Freshmen';
+  final profileMajor = 'Computer Science';
+  List profileCourses = [
+    'CS 1200',
+    'CS 2305',
+    'CS 2337',
+    'RHET 1302',
+    'GOVT 2306',
+    'ECS 1100',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +38,10 @@ class _ProfilePageState extends State<ProfilePage> {
                 _profileImage(),
                 const SizedBox(height: 32),
                 _profileName(),
+                _profileMajor(),
+                _profileClassification(),
+                const SizedBox(height: 32),
+                _signOutButton(),
               ],
             ),
           ),
@@ -43,5 +59,28 @@ class _ProfilePageState extends State<ProfilePage> {
 
   Widget _profileName() {
     return Text(profileName, style: Theme.of(context).textTheme.headline2);
+  }
+
+  Widget _profileClassification() {
+    return Text(profileClassification,
+        style: Theme.of(context).textTheme.subtitle1);
+  }
+
+  Widget _profileMajor() {
+    return Text(profileMajor, style: Theme.of(context).textTheme.headline3);
+  }
+
+  Widget _signOutButton() {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        child: const Text('Sign Out'),
+        onPressed: () {
+          AuthService authService =
+              Provider.of<AuthService>(context, listen: false);
+          authService.signOut();
+        },
+      ),
+    );
   }
 }
