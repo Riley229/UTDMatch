82dfaf7e7e79e3d91b073256531a5ac21ca68aa7
Anthuony <brianpham907@gmail.com>
2021-11-14 10:03:14 -0600

    started showing liked tutors

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,16 +13,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    List<AppUser> tutors = const [];
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 64),
-        child: Center(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 64),
           child: Consumer<AppUser?>(
             builder: (context, appUser, child) {
               return Column(
                 mainAxisSize: MainAxisSize.max,
                 children: [
+                  if (tutors.isNotEmpty) const Divider(),
+                  if (tutors.isNotEmpty) _currentTutors(),
+                  if (tutors.isNotEmpty) _tutors(tutors),
                   const Divider(),
                   _findTutors(),
                   _selectACourse(),
@@ -46,28 +51,45 @@ class _HomePageState extends State<HomePage> {
         style: Theme.of(context).textTheme.headline4);
   }
 
+  Widget _currentTutors() {
+    return Text('Current Tutors', style: Theme.of(context).textTheme.headline2);
+  }
+
+  Widget _tutors(List<AppUser> tutors) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const ClampingScrollPhysics(),
+      itemCount: tutors.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          leading: const Icon(Icons.book),
+          title: tutors.elementAt(index).avatar,
+          onTap: () {},
+        );
+      },
+    );
+  }
+
   Widget _courses(Map<String, int> courses) {
-    return Expanded(
-      child: ListView.builder(
-        shrinkWrap: true,
-        physics: const ClampingScrollPhysics(),
-        itemCount: courses.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            leading: const Icon(Icons.book),
-            title: Text(courses.keys.elementAt(index)),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      CoursePage(course: courses.keys.elementAt(index)),
-                ),
-              );
-            },
-          );
-        },
-      ),
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const ClampingScrollPhysics(),
+      itemCount: courses.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          leading: const Icon(Icons.book),
+          title: Text(courses.keys.elementAt(index)),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    CoursePage(course: courses.keys.elementAt(index)),
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
